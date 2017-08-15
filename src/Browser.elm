1717c3e09b6ae04f1052513a31a36cfdb910ff7b
Evan Czaplicki <evancz@users.noreply.github.com>
2017-08-14 23:40:40 -0700

    Initial commit with potential API

---
+++ src/Browser.elm
@@ -0,0 +1,125 @@
+module Browser exposing
+  ( Env
+  , program
+  , Url
+  , load
+  , reload
+  , reloadAndSkipCache
+  )
+
+{-|
+
+# Create Programs
+@docs Env, program, Url
+
+# Page Loads
+@docs load, reload, reloadAndSkipCache
+
+-}
+
+
+import Elm.Kernel.Browser
+import Task
+import VirtualCss
+import VirtualDom
+
+
+
+-- PROGRAMS
+
+
+type alias Env flags =
+  { url : Url
+  , flags : flags
+  }
+
+
+program :
+  { init : Env flags -> (model, Cmd msg)
+  , view : model -> { title : String, css : VirtualCss.Css, html : VirtualDom.Node msg }
+  , update : msg -> model -> ( model, Cmd msg )
+  , onNavigation : Url -> msg
+  , subscriptions : model -> Sub msg
+  }
+  -> Program flags model msg
+
+
+
+-- URL
+
+
+{-| A bunch of information about the URL in the address bar. You should always
+be using the [`elm-lang/url`][url] package to turn these URLs into nice Elm
+data. Check out the [`Url.Parser`][parser] module in particular.
+
+**Note:** The fields correspond with the fields in `document.location` as
+described [here](https://developer.mozilla.org/en-US/docs/Web/API/Url).
+-}
+type alias Url =
+  { href : String
+  , host : String
+  , hostname : String
+  , protocol : String
+  , origin : String
+  , port_ : String
+  , pathname : String
+  , search : String
+  , hash : String
+  , username : String
+  , password : String
+  }
+
+
+
+-- PAGE LOADS
+
+
+{-| Leave the current page and load the given URL. **This always results in a
+page load**, even if the provided URL is the same as the current one.
+
+    load "http://elm-lang.org"
+
+Check out the [`elm-lang/url`][url] package for help building URLs. The
+[`Url.absolute`][abs] and [`Url.relative`][rel] functions can be particularly
+handy!
+
+[url]: http://package.elm-lang.org/packages/elm-lang/url/latest
+[abs]: http://package.elm-lang.org/packages/elm-lang/url/latest/Url#absolute
+[rel]: http://package.elm-lang.org/packages/elm-lang/url/latest/Url#relative
+
+Check out the [`Browser.History`](Browser-History) module if you want
+page transitions that are faster and prettier, like in a “single-page app”.
+-}
+load : String -> Cmd msg
+load url =
+  Task.perform never (Elm.Kernel.Browser.load url)
+
+
+{-| Reload the current page. **This always results in a page load!**
+This may grab resources from the browser cache, so use
+[`reloadAndSkipCache`](reloadAndSkipCache) if you want to be sure
+that you are not loading any cached resources.
+-}
+reload : Cmd msg
+reload =
+  Task.perform never (Elm.Kernel.Browser.reload False)
+
+
+{-| Reload the current page without using the browser cache. **This always
+results in a page load!** It is more common to want [`reload`](reload).
+-}
+reloadAndSkipCache : Cmd msg
+reloadAndSkipCache =
+  Task.perform never (Elm.Kernel.Browser.reload True)
+
+
+
+-- NEVER
+
+
+type Never = JustOneMore Never
+
+
+never : Never -> a
+never (JustOneMore nvr) =
+  never nvr
---
+++ src/Browser/History.elm
@@ -0,0 +1,127 @@
+module Browser.History exposing
+  ( push
+  , replace
+  , back
+  , forward
+  )
+
+
+{-| This module helps you manage browser history yourself with [`push`](#push),
+[`replace`](#replace), [`back`](#back), and [`forward`](#forward).
+
+The most important function is [`push`](#push) which changes the address bar
+*without* starting a page load.
+
+
+## What is a page load?
+
+  1. Request a new HTML document. The page goes blank.
+  2. As the HTML loads, request any `<script>` or `<link>` resources.
+  3. A `<script>` may mutate the document, so these tags block rendering.
+  4. When *all* of the assets are loaded, actually render the page.
+
+That means the page will go blank for at least two round-trips to the servers!
+You may have 90% of the data you need and be blocked on a font that is taking
+a long time. Still blank!
+
+
+## How does `push` help?
+
+The `push` function changes the URL, but lets you keep the current HTML. This
+means the page *never* goes blank. Instead of making two round-trips to the
+server, you load whatever assets you want from within Elm. Maybe you do not
+need any round-trips! Meanwhile, you retain full control over the UI, so you
+can show a loading bar, show information as it loads, etc. Whatever you want!
+
+
+# History
+@docs push, replace, back, forward
+
+-}
+
+
+import Elm.Kernel.Browser
+import Task
+
+
+
+-- CHANGE HISTORY
+
+
+{-| Change the URL, but do not trigger a page load.
+
+This will add a new entry to the browser history. **(DEFAULT)**
+
+Check out the [`elm-lang/url`][url] package for help building URLs. The
+[`Url.absolute`][abs] and [`Url.relative`][rel] functions can be particularly
+handy!
+
+[url]: http://package.elm-lang.org/packages/elm-lang/url/latest
+[abs]: http://package.elm-lang.org/packages/elm-lang/url/latest/Url#absolute
+[rel]: http://package.elm-lang.org/packages/elm-lang/url/latest/Url#relative
+
+**Note:** If the user has gone `back` a few pages, there will be &ldquo;future
+pages&rdquo; that the user can go `forward` to. Adding a new URL in that
+scenario will clear out any future pages. It is like going back in time and
+making a different choice.
+-}
+push : String -> Cmd msg
+push url =
+  Task.perform never (Elm.Kernel.Browser.push url)
+
+
+{-| Change the URL, but do not trigger a page load.
+
+This *will not* add a new entry to the browser history. **(WEIRD)**
+
+This can be useful if you have search box and you want the `?search=hats` in
+the URL to match without adding a history entry for every single key stroke.
+Imagine how annoying it would be to click `back` thirty times and still be on
+the same page!
+-}
+replace : String -> Cmd msg
+replace url =
+  Task.perform never (Elm.Kernel.Browser.replace url)
+
+
+
+-- NAVIGATE HISTORY
+
+
+{-| Go back some number of pages. So `back 1` goes back one page, and `back 2`
+goes back two pages.
+
+**Note:** You only manage the browser history that *you* created. Think of this
+library as letting you have access to a small part of the overall history. So
+if you go back farther than the history you own, you will just go back to some
+other website!
+-}
+back : Int -> Cmd msg
+back n =
+  Task.perform never (Elm.Kernel.Browser.jump -n)
+
+
+{-| Go forward some number of pages. So `forward 1` goes forward one page, and
+`forward 2` goes forward two pages. If there are no more pages in the future,
+this will do nothing.
+
+**Note:** You only manage the browser history that *you* created. Think of this
+library as letting you have access to a small part of the overall history. So
+if you go forward farther than the history you own, the user will end up on
+whatever website they visited next!
+-}
+forward : Int -> Cmd msg
+forward n =
+  Task.perform never (Elm.Kernel.Browser.jump n)
+
+
+
+-- NEVER
+
+
+type Never = JustOneMore Never
+
+
+never : Never -> a
+never (JustOneMore nvr) =
+  never nvr
