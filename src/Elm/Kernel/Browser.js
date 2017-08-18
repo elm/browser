c267b2e23f8ab617212b0525e299e4889eed9181
Evan Czaplicki <evancz@users.noreply.github.com>
2017-08-17 18:14:15 -0700

    Implement draft of kernel code

---
+++ src/Elm/Kernel/Browser.js
@@ -0,0 +1,235 @@
+/*
+
+import Browser exposing (Env)
+import Json.Decode as Json exposing (map)
+import Elm.Kernel.List exposing (Nil)
+import Elm.Kernel.Scheduler exposing (binding, succeed)
+import Elm.Kernel.Utils exposing (Tuple0)
+import Elm.Kernel.VirtualCss exposing (none, diff)
+import Elm.Kernel.VirtualDom exposing (appendChild, applyFacts, diff, doc, render)
+
+*/
+
+
+// FAKE NAVIGATION
+
+
+function _Browser_go(n)
+{
+	return __Scheduler_binding(function(callback)
+	{
+		if (n !== 0)
+		{
+			history.go(n);
+		}
+		callback(__Scheduler_succeed(__Utils_Tuple0));
+	});
+}
+
+
+function _Browser_push(url)
+{
+	return __Scheduler_binding(function(callback)
+	{
+		history.pushState({}, '', url);
+		callback(__Scheduler_succeed(_Browser_getUrl()));
+	});
+}
+
+
+function _Browser_replace(url)
+{
+	return __Scheduler_binding(function(callback)
+	{
+		history.replaceState({}, '', url);
+		callback(__Scheduler_succeed(_Browser_getUrl()));
+	});
+}
+
+
+// REAL NAVIGATION
+
+function _Browser_reloadPage(skipCache)
+{
+	return __Scheduler_binding(function(callback)
+	{
+		_VirtualDom_doc.location.reload(skipCache);
+		callback(__Scheduler_succeed(__Utils_Tuple0));
+	});
+}
+
+function _Browser_setLocation(url)
+{
+	return __Scheduler_binding(function(callback)
+	{
+		try
+		{
+			window.location = url;
+		}
+		catch(err)
+		{
+			// Only Firefox can throw a NS_ERROR_MALFORMED_URI exception here.
+			// Other browsers reload the page, so let's be consistent about that.
+			_VirtualDom_doc.location.reload(false);
+		}
+		callback(__Scheduler_succeed(__Utils_Tuple0));
+	});
+}
+
+
+
+// GET URL
+
+
+function _Browser_getUrl()
+{
+	var loc = _VirtualDom_doc.location;
+
+	return {
+		__$href: loc.href,
+		__$host: loc.host,
+		__$hostname: loc.hostname,
+		__$protocol: loc.protocol,
+		__$origin: loc.origin,
+		__$port_: loc.port,
+		__$pathname: loc.pathname,
+		__$search: loc.search,
+		__$hash: loc.hash,
+		__$username: loc.username,
+		__$password: loc.password
+	};
+}
+
+
+
+// DETECT IE11 PROBLEMS
+
+
+function _Browser_isInternetExplorer11()
+{
+	return window.navigator.userAgent.indexOf('Trident') !== -1;
+}
+
+
+
+// PROGRAMS
+
+
+var _Browser_staticPage = F5(function(virtualNode, flagDecoder, object, moduleName, debugMetadata)
+{
+	object['staticPage'] = function staticPage(node)
+	{
+		node.parentNode.replaceChild(
+			__VirtualDom_render(virtualNode, function() {}),
+			node
+		);
+	};
+});
+
+
+var _Browser_embed = F5(function(impl, flagDecoder, object, moduleName, debugMetadata)
+{
+	object['embed'] = function embed(node, flags)
+	{
+		__Platform_initialize(
+			moduleName,
+			flagDecoder,
+			flags,
+			impl.__$init,
+			impl.__$update,
+			impl.__$subscriptions,
+			_Browser_makeStepperBuilder(node, impl.__$view)
+		);
+	};
+});
+
+
+var _Browser_fullscreen = F5(function(impl, flagDecoder, object, moduleName, debugMetadata)
+{
+	object['fullscreen'] = function fullscreen(flags)
+	{
+		var title = _VirtualDom_doc.title;
+		var virtualCss = __VirtualCss_none;
+
+		var style = _VirtualDom_doc.createElement('style');
+		_VirtualDom_appendChild(style, _VirtualDom_doc.createTextNode(''));
+		_VirtualDom_appendChild(_VirtualDom_doc.head, style);
+
+		__Platform_initialize(
+			moduleName,
+			A2(__Json_map, __Browser_Env(_Browser_getUrl()), flagDecoder),
+			flags,
+			impl.__$init,
+			impl.__$update,
+			impl.__$subscriptions,
+			_Browser_makeStepperBuilder(_VirtualDom_doc.body, function(model) {
+				var ui = impl.__$view(model);
+				if (title !== ui.__$title)
+				{
+					_VirtualDom_doc.title = title = ui.title;
+				}
+				__VirtualCss_diff(style.sheet, virtualCss, virtualCss = ui.__$css);
+				return F3(__VirtualDom_node, 'body', __List_Nil, ui.__$body);
+			}
+		);
+	};
+});
+
+
+
+// RENDERER
+
+
+function _Browser_makeStepperBuilder(domNode, view)
+{
+	return function(sendToApp, initialModel)
+	{
+		var currNode = _VirtualDom_virtualize(domNode);
+
+		return _Browser_makeAnimator(initialModel, function(model)
+		{
+			var nextNode = view(model);
+			var patches = __VirtualDom_diff(currNode, nextNode);
+			domNode = __VirtualDom_applyPatches(domNode, currNode, patches, sendToApp);
+			currNode = nextNode;
+		});
+	};
+}
+
+
+
+// ANIMATION
+
+
+var _Browser_requestAnimationFrame =
+	typeof requestAnimationFrame !== 'undefined'
+		? requestAnimationFrame
+		: function(callback) { setTimeout(callback, 1000 / 60); };
+
+
+function _Browser_makeAnimator(model, draw)
+{
+	draw(model);
+
+	var state = __4_NO_REQUEST;
+
+	function updateIfNeeded()
+	{
+		state = state === __4_EXTRA_REQUEST
+			? __4_NO_REQUEST
+			: ( _Browser_requestAnimationFrame(updateIfNeeded), draw(model), __4_EXTRA_REQUEST );
+	}
+
+	return function(nextModel, isSync)
+	{
+		model = nextModel;
+
+		isSync
+			? ( draw(model),
+				state === __4_PENDING_REQUEST && state = __4_EXTRA_REQUEST
+				)
+			: ( state === __4_NO_REQUEST && _Browser_requestAnimationFrame(updateIfNeeded),
+				state = __4_PENDING_REQUEST
+				);
+	};
+}
