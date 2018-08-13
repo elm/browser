5aa46ff1c6d47f236a3d6e96a2fbf98e10dcf1ca
Logan McPhail <logan.airnomad@gmail.com>
2018-08-12 20:42:58 -0600

    fix and update examples

--- examples/drag.elm
+++ examples/drag.elm
@@ -1,113 +1,108 @@
-import Browser.Mouse as Mouse
+module Main exposing (..)
+
+import Browser
+import Browser.Events
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-
+import Json.Decode as D
 
 
 -- MAIN
 
 
 main =
-  Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
 
 
 -- MODEL
 
 
-type Model =
-  { x : Int
-  , y : Int
-  , dragState : DragState
-  }
+type alias Model =
+    { x : Int
+    , y : Int
+    , dragState : DragState
+    }
 
 
 type DragState
-  = Static
-  | Moving Int Int Int Int
+    = Static
+    | Moving { startX : Int, startY : Int, endX : Int, endY : Int }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-  ( Model 100 100 Static
-  , Cmd.none
-  )
+    ( { x = 0, y = 0, dragState = Static }, Cmd.none )
 
 
 
 -- UPDATE
 
 
+type alias Point =
+    { x : Int, y : Int }
+
+
 type Msg
-  = Start Int Int
-  | Move Int Int
-  | Stop Int Int
+    = Start Point
+    | Move Point
+    | Stop Point
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+fromPoint : (Point -> Msg) -> Int -> Int -> Msg
+fromPoint msg x y =
+    Point x y |> msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Start x y ->
-      ( { model | dragState = Moving x y x y }
-      , Cmd.none
-      )
+    case msg of
+        Start { x, y } ->
+            ( { model
+                | dragState =
+                    Moving
+                        { startX = x
+                        , startY = y
+                        , endX = x
+                        , endY = y
+                        }
+              }
+            , Cmd.none
+            )
 
-    Move x y ->
-      case model.dragState of
-        Static ->
-          ( model, Cmd.none )
+        Move { x, y } ->
+            case model.dragState of
+                Static ->
+                    ( model, Cmd.none )
 
-        Moving startX startY _ _ ->
-          ( { model | dragState = Moving startX startY x y }
-          )
+                Moving { startX, startY } ->
+                    ( { model
+                        | dragState =
+                            Moving
+                                { startX = startX
+                                , startY = startY
+                                , endX = x
+                                , endY = y
+                                }
+                      }
+                    , Cmd.none
+                    )
 
-    Stop x y ->
-      case model.dragState of
-        Static ->
-          ( model, Cmd.none )
+        Stop { x, y } ->
+            case model.dragState of
+                Static ->
+                    ( model, Cmd.none )
 
-        Moving startX startY _ _ ->
-          ( Model (model.x + startX - x) (model.y + startY - y) Static
-          , Cmd.none
-          )
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-  let
-    (x, y) = getPosition model
-  in
-  div
-    [ style "background-color" "rgb(104,216,239)"
-    , style "position" "absolute"
-    , style "top"  (String.fromInt x ++ "px")
-    , style "left" (String.fromInt y ++ "px")
-    , style "width" "100px"
-    , style "height" "100px"
-    , on "mousedown" (D.map2 Start pageX pageY)
-    , on "mouseup" (D.map2 Stop pageX pageY)
-    ]
-    [ text "Drag me!"
-    ]
-
-
-getPosition : Model -> (Int, Int)
-getPosition model =
-  case model.dragState of
-    Static ->
-      (model.x, model.y)
-
-    Moving startX startY endX endY ->
-      (x + startX - endX, y + startY - endY)
+                Moving { startX, startY } ->
+                    ( Model (model.x - startX + x) (model.y - startY + y) Static
+                    , Cmd.none
+                    )
 
 
 
@@ -116,19 +111,64 @@ getPosition model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  case model.dragState of
-    Static ->
-      Sub.none
+    case model.dragState of
+        Static ->
+            Sub.none
 
-    Moving _ _ _ _ ->
-      Mouse.moves (D.map2 Move pageX pageY)
+        Moving _ ->
+            Browser.Events.onMouseMove (D.map2 (fromPoint Move) pageX pageY)
 
 
 pageX : D.Decoder Int
 pageX =
-  D.field "pageX" D.int
+    D.field "pageX" D.int
 
 
 pageY : D.Decoder Int
 pageY =
-  D.field "pageY" D.int
+    D.field "pageY" D.int
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        ( x, y ) =
+            getPosition model
+    in
+    div
+        [ style "width" "100vw"
+        , style "height" "100vh"
+        , on "mouseup" (D.map2 (fromPoint Stop) pageX pageY)
+        ]
+        [ div
+            [ style "background-color" "rgb(104,216,239)"
+            , style "position" "absolute"
+            , style "left" (String.fromInt x ++ "px")
+            , style "top" (String.fromInt y ++ "px")
+            , style "width" "100px"
+            , style "height" "100px"
+            , style "padding" "20px"
+            , style "text-align" "center"
+            , on "mousedown" (D.map2 (fromPoint Start) pageX pageY)
+            ]
+            [ text "Drag me!"
+            , br [] []
+            , text <| "x: " ++ String.fromInt x
+            , br [] []
+            , text <| "y: " ++ String.fromInt y
+            ]
+        ]
+
+
+getPosition : Model -> ( Int, Int )
+getPosition model =
+    case model.dragState of
+        Static ->
+            ( model.x, model.y )
+
+        Moving m ->
+            ( model.x - m.startX + m.endX, model.y - m.startY + m.endY )
--- examples/wasd.elm
+++ examples/wasd.elm
@@ -1,7 +1,22 @@
+module Main exposing (..)
 
-import Browser.Keyboard as Keyboard
-import Browser.Window as Window
+import Browser
+import Browser.Events
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Json.Decode as D
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
@@ -9,23 +24,31 @@ import Json.Decode as D
 
 
 type alias Model =
-  { x : Float
-  , y : Float
-  , north : KeyStatus
-  , south : KeyStatus
-  , east : KeyStatus
-  , west : KeyStatus
-  }
+    { x : Float
+    , y : Float
+    , north : KeyStatus
+    , south : KeyStatus
+    , east : KeyStatus
+    , west : KeyStatus
+    }
 
 
-type KeyStatus = Up | Down
+type KeyStatus
+    = Up
+    | Down
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-  ( Model 0 0 Up Up Up Up
-  , Cmd.none
-  )
+    ( { x = 0
+      , y = 0
+      , north = Up
+      , south = Up
+      , east = Up
+      , west = Up
+      }
+    , Cmd.none
+    )
 
 
 
@@ -33,62 +56,86 @@ init _ =
 
 
 type Msg
-  = Change KeyStatus String
-  | Blur
-  | TimeDelta Float
+    = Change KeyStatus String
+    | Blur
+    | TimeDelta Float
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Change status string ->
-      ( updateKey status string
-      , Cmd.none
-      )
+    case msg of
+        Change status string ->
+            ( updateKey status string model
+            , Cmd.none
+            )
 
-    Blur ->
-      ( Model model.x model.y Up Up Up Up
-      , Cmd.none
-      )
+        Blur ->
+            ( { x = model.x
+              , y = model.y
+              , north = Up
+              , south = Up
+              , east = Up
+              , west = Up
+              }
+            , Cmd.none
+            )
 
-    TimeDelta delta ->
-      ( updatePosition delta model
-      , Cmd.none
-      )
+        TimeDelta delta ->
+            ( updatePosition delta model
+            , Cmd.none
+            )
 
 
 updateKey : KeyStatus -> String -> Model -> Model
 updateKey status string model =
-  case string of
-    "w" -> { model | north = status }
-    "a" -> { model | east  = status }
-    "s" -> { model | south = status }
-    "d" -> { model | west  = status }
-    _   -> model
+    case string of
+        "w" ->
+            { model | north = status }
+
+        "a" ->
+            { model | east = status }
+
+        "s" ->
+            { model | south = status }
+
+        "d" ->
+            { model | west = status }
+
+        _ ->
+            model
 
 
 updatePosition : Float -> Model -> Model
 updatePosition delta model =
-  let
-    vx = toOne model.east - toOne model.west
-    vy = toOne model.north - toOne model.south
-  in
-  { model
-      | x = model.x + vx * delta
-      , y = model.y + vy * delta
-  }
+    let
+        vx =
+            toOne model.east - toOne model.west
+
+        vy =
+            toOne model.north - toOne model.south
+    in
+    { model
+        | x = model.x - vx * delta
+        , y = model.y - vy * delta
+    }
 
 
 toOne : KeyStatus -> Float
 toOne status =
-  if isDown status then 1 else 0
+    if isDown status then
+        1
+    else
+        0
 
 
 isDown : KeyStatus -> Bool
 isDown status =
-  case status of
-    Down -> True
-    Up -> False
+    case status of
+        Down ->
+            True
+
+        Up ->
+            False
 
 
 
@@ -97,28 +144,28 @@ isDown status =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.batch
-    [ Keyboard.downs (D.map (Change Down) keyDecoder)
-    , Keyboard.ups (D.map (Change Up) keyDecoder)
-    , Window.blurs (D.succeed Blur)
-    , if anyIsDown then
-        Animation.deltas TimeDelta
-      else
-        Sub.none
-    ]
+    Sub.batch
+        [ Browser.Events.onKeyDown (D.map (Change Down) keyDecoder)
+        , Browser.Events.onKeyUp (D.map (Change Up) keyDecoder)
+        , Browser.Events.onVisibilityChange (\_ -> Blur)
+        , if anyIsDown model then
+            Browser.Events.onAnimationFrameDelta TimeDelta
+          else
+            Sub.none
+        ]
 
 
 keyDecoder : D.Decoder String
 keyDecoder =
-  D.field "key" D.string
+    D.field "key" D.string
 
 
 anyIsDown : Model -> Bool
 anyIsDown model =
-  isDown model.north
-  || isDown model.south
-  || isDown model.east
-  || isDown model.west
+    isDown model.north
+        || isDown model.south
+        || isDown model.east
+        || isDown model.west
 
 
 
@@ -127,13 +174,13 @@ anyIsDown model =
 
 view : Model -> Html Msg
 view model =
-  div
-    [ style "background-color" "rgb(104,216,239)"
-    , style "position" "absolute"
-    , style "top"  (String.fromInt (round model.x) ++ "px")
-    , style "left" (String.fromInt (round model.y) ++ "px")
-    , style "width" "100px"
-    , style "height" "100px"
-    ]
-    [ text "Press WASD keys!"
-    ]
+    div
+        [ style "background-color" "rgb(104,216,239)"
+        , style "position" "absolute"
+        , style "left" (String.fromInt (round model.x) ++ "px")
+        , style "top" (String.fromInt (round model.y) ++ "px")
+        , style "width" "100px"
+        , style "height" "100px"
+        ]
+        [ text "Press WASD keys!"
+        ]
